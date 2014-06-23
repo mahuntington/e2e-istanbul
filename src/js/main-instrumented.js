dbda89c2f060b7c696d697db3b1080246804e916
Matt Huntington <Matt.Huntington@akqa.com>
2014-06-23 18:06:37 -0400

    instrumented code

---
+++ src/js/main-instrumented.js
@@ -0,0 +1,9 @@
+
+var __cov_Hq55tQjQYx3tAA6SCTbFyg = (Function('return this'))();
+if (!__cov_Hq55tQjQYx3tAA6SCTbFyg.__coverage__) { __cov_Hq55tQjQYx3tAA6SCTbFyg.__coverage__ = {}; }
+__cov_Hq55tQjQYx3tAA6SCTbFyg = __cov_Hq55tQjQYx3tAA6SCTbFyg.__coverage__;
+if (!(__cov_Hq55tQjQYx3tAA6SCTbFyg['/Users/matt.huntington/Documents/Sites/e2e-with-coverage/src/js/main.js'])) {
+   __cov_Hq55tQjQYx3tAA6SCTbFyg['/Users/matt.huntington/Documents/Sites/e2e-with-coverage/src/js/main.js'] = {"path":"/Users/matt.huntington/Documents/Sites/e2e-with-coverage/src/js/main.js","s":{"1":0,"2":0,"3":0,"4":0},"b":{"1":[0,0]},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":1,"column":69}},"2":{"start":{"line":2,"column":0},"end":{"line":6,"column":1}},"3":{"start":{"line":3,"column":1},"end":{"line":3,"column":17}},"4":{"start":{"line":5,"column":1},"end":{"line":5,"column":17}}},"branchMap":{"1":{"line":2,"type":"if","locations":[{"start":{"line":2,"column":0},"end":{"line":2,"column":0}},{"start":{"line":2,"column":0},"end":{"line":2,"column":0}}]}},"code":["(function () { document.getElementsByTagName('h1')[0].innerHTML = \"changed with js\";","if(false){","\tvar foo = \"bar\";","} else {","\tvar bar = \"foo\";","}","}());"]};
+}
+__cov_Hq55tQjQYx3tAA6SCTbFyg = __cov_Hq55tQjQYx3tAA6SCTbFyg['/Users/matt.huntington/Documents/Sites/e2e-with-coverage/src/js/main.js'];
+__cov_Hq55tQjQYx3tAA6SCTbFyg.s['1']++;document.getElementsByTagName('h1')[0].innerHTML='changed with js';__cov_Hq55tQjQYx3tAA6SCTbFyg.s['2']++;if(false){__cov_Hq55tQjQYx3tAA6SCTbFyg.b['1'][0]++;__cov_Hq55tQjQYx3tAA6SCTbFyg.s['3']++;var foo='bar';}else{__cov_Hq55tQjQYx3tAA6SCTbFyg.b['1'][1]++;__cov_Hq55tQjQYx3tAA6SCTbFyg.s['4']++;var bar='foo';}
