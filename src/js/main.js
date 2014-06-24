7298ed3a03e894b6de741706a539f5ce48c96faa
Matt Huntington <Matt.Huntington@akqa.com>
2014-06-24 10:46:26 -0400

    switching to jquery js code generated coverage

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-document.getElementsByTagName('h1')[0].innerHTML = "changed with js";
-if(false){
-	var foo = "bar";
-} else {
-	var bar = "foo";
-}
+(function($){
+	$(function(){
+		$('h1').click(function(){
+			$('body').append('<div>added through jquery</div>');
+		})
+	});
+})(jQuery);
