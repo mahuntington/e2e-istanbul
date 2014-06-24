2e4eaa0000500bcbe5ed5f24708ef5fc5ab8577d
Matt Huntington <Matt.Huntington@akqa.com>
2014-06-24 12:21:11 -0400

    test using jQuery and coverage passes and covers all!

--- specs/test-spec.js
+++ specs/test-spec.js
@@ -1,16 +1,28 @@
 var webdriver = require('selenium-webdriver');
 var fs = require('fs');
+jasmine.getEnv().addReporter(new jasmine.ConsoleReporter(console.log));
 
 var driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.chrome()).
     build();
 
-describe('basic test', function () {
-	it('should be on correct page', function (done) {
+describe('Main page', function () {
+	it('should have title of Test Title', function (done) {
 		driver.get('http://localhost:9001/');
 		driver.getTitle().then(function(title) {
 			expect(title).toBe('Test Title');
 		});
+		done();
+	});
+	it('should show extra div when clicking on link', function (done) {
+		driver.findElement(webdriver.By.id('clicker')).click();
+		driver.findElement(webdriver.By.id('jQueryAddedDiv')).then(function(element){
+			element.getText().then(function(div_text){
+				expect(div_text).toBe('added through jquery');
+				done();
+			});
+		});
+				
 	    driver.switchTo().defaultContent();
 	    driver.executeScript("return window.__coverage__;").then(function (obj) {
 			fs.writeFile('coverage/coverage.json', JSON.stringify(obj));
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 (function($){
 	$(function(){
-		$('h1').click(function(){
-			$('body').append('<div>added through jquery</div>');
-		})
+		$('a').click(function(){
+			$('body').append('<div id="jQueryAddedDiv">added through jquery</div>');
+		});
 	});
 })(jQuery);
