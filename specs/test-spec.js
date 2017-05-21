09db57b945188c73abb7c398637a13e4e89dc1ac
Matt Huntington <matt.huntington@gmail.com>
2017-05-21 15:57:41 -0400

    got tests to work again

--- specs/test-spec.js
+++ specs/test-spec.js
@@ -1,32 +1,61 @@
 var webdriver = require('selenium-webdriver');
-var fs = require('fs');
-jasmine.getEnv().addReporter(new jasmine.ConsoleReporter(console.log));
 
-var driver = new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.chrome()).
-    build();
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
 
-describe('Main page', function () {
-	driver.get('http://localhost:9001/');
-	it('should have title of Test Title', function (done) {
-		driver.getTitle().then(function(title) {
-			expect(title).toBe('Test Title');
-		});
-		done();
-	});
-	it('should show extra div when clicking on link', function (done) {
-		driver.findElement(webdriver.By.id('clicker')).click();
-		driver.findElement(webdriver.By.id('jQueryAddedDiv')).then(function(element){
-			element.getText().then(function(div_text){
-				expect(div_text).toBe('added through jquery');
-				done();
-			});
-		});
-				
-	    driver.switchTo().defaultContent();
-	    driver.executeScript("return window.__coverage__;").then(function (obj) {
-			fs.writeFile('coverage/coverage.json', JSON.stringify(obj));
-			driver.quit();
-		});
-	});
+// var fs = require('fs');
+// jasmine.getEnv().addReporter(new jasmine.ConsoleReporter(console.log));
+//
+// describe('Main page', function () {
+// 	driver.get('http://localhost:9001/');
+// 	it('should have title of Test Title', function (done) {
+// 		driver.getTitle().then(function(title) {
+// 			expect(title).toBe('Test Title');
+//          done();
+// 		});
+// 	});
+// 	it('should show extra div when clicking on link', function (done) {
+// 		driver.findElement(webdriver.By.id('clicker')).click();
+// 		driver.findElement(webdriver.By.id('jQueryAddedDiv')).then(function(element){
+// 			element.getText().then(function(div_text){
+// 				expect(div_text).toBe('added through jquery');
+//                 driver.switchTo().defaultContent();
+//         	    driver.executeScript("return window.__coverage__;").then(function (obj) {
+//         			fs.writeFile('coverage/coverage.json', JSON.stringify(obj));
+//         			driver.quit();
+//         		});
+//                 done();
+// 			});
+// 		});
+// 	});
+// });
+//
+//
+describe('Main page', function (){
+    it('should have title of Test Title', function (done) {
+        driver.get('http://localhost:9001/').then(function(){
+            driver.wait(webdriver.until.titleIs('Test Title'), 10000);
+            driver.getTitle().then(function(title) {
+                expect(title).toBe('Test Title');
+                done();
+            });
+        });
+    });
+    it('should show extra div when clicking on link', function (done) {
+        driver.findElement(webdriver.By.id('clicker')).click();
+        driver.findElement(webdriver.By.id('jQueryAddedDiv')).then(function(element){
+            element.getText().then(function(div_text){
+                expect(div_text).toBe('added through jquery');
+                // driver.switchTo().defaultContent();
+                // driver.executeScript("return window.__coverage__;").then(function (obj) {
+                //     fs.writeFile('coverage/coverage.json', JSON.stringify(obj));
+                //     driver.quit();
+                //     done();
+                // });
+                driver.quit();
+                done();
+            });
+        });
+    });
 });
